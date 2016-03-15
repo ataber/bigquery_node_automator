811313a7a095fe083b28e7dd3d03cdf67b1cb04f
Andrew Taber <andrew.e.taber@gmail.com>
2016-03-15 13:35:53 -0700

    Replace newlines with spaces.

--- run_query.js
+++ run_query.js
@@ -7,8 +7,8 @@ fs.readFile(process.argv[2], 'utf8', (err, data) => {
     if (!err) {
       job.getQueryResults((err,rows) => {
         if (!err) {
-          let firsts = rows.map((d) => {return JSON.stringify(d['first_body'])})
-          let lasts = rows.map((d) => {return JSON.stringify(d['last_body'])})
+          let firsts = rows.map((d) => {return d['first_body'].replace(/\n/g," ")})
+          let lasts = rows.map((d) => {return d['last_body'].replace(/\n/g," ")})
           fs.appendFileSync("firsts.txt", firsts.join("\n"))
           fs.appendFileSync("lasts.txt", lasts.join("\n"))
         }
